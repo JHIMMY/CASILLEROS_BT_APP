b8a90f0b46acd7d183670589d98a02b386f7044e
JHIMMY ASTORAQUE DURAN <jadsa.corp2@gmail.com>
2018-08-18 19:30:04 -0400

    setting plugins and bt connection

---
+++ LOCKERS_APP/www/js/app.js
@@ -0,0 +1,47 @@
+
+if ('addEventListener' in document){
+    document.addEventListener('DOMContentLoaded', function (){
+        FastClick.attach(document.body);
+        app.initialize();
+    }, false);
+}
+
+let macAddress = "20:16:11:29:61:79";
+
+let app = {
+    initialize: function() {
+        this.bindEvents();
+        // this.iniciaBotones();
+    },
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    onDeviceReady: function() {
+        bluetoothSerial.connect(macAddress, app.onConnect, app.onDisconnect);
+    },
+    onConnect: function() {
+        bluetoothSerial.subscribe("\n", app.onMessage, app.subscribeFailed);
+        statusDiv.innerHTML="Connected to " + macAddress + ".";        
+    }, 
+    onDisconnect: function() {
+        alert("Disconnected");
+        statusDiv.innerHTML="Disconnected.";
+    },
+    onMessage: function(data) {
+        counter.innerHTML = data;        
+    },
+    subscribeFailed: function() {
+        alert("subscribe failed");
+    },
+
+    // iniciaBotones: () => {
+    //     let connectButton = document.getElementById("btn1");
+
+    //     connectButton.addEventListener('click', this.contectarModuloBT, false);
+    // },
+
+    // contectarModuloBT: () => {
+    //     console.log("Intrentando Conectar ....");
+    //     bluetoothSerial.connect(macAddress_or_uuid, connectSuccess, connectFailure);
+    // }
+};
