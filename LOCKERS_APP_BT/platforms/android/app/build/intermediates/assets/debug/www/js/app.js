f6144e241e4c47e37d38f68280709cd7ea5a154f
JHIMMY ASTORAQUE DURAN <jadsa.corp2@gmail.com>
2018-08-19 13:23:29 -0400

    Working 0.2.1 activar button working and statuslabel

--- LOCKERS_APP_BT/platforms/android/app/build/intermediates/assets/debug/www/js/app.js
+++ LOCKERS_APP_BT/platforms/android/app/build/intermediates/assets/debug/www/js/app.js
@@ -11,25 +11,57 @@ let app = {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     onDeviceReady: function() {
-        bluetoothSerial.connect(macAddress, app.onConnect, app.onDisconnect);
+        // bluetoothSerial.connect(macAddress, app.onConnect, app.onDisconnect);
+        app.setListeners();
     },
     onConnect: function() {
+        activateAllSystems();
         bluetoothSerial.subscribe("\n", app.onMessage, app.subscribeFailed);
-        statusDiv.innerHTML="Connected to " + macAddress + ".";        
+        // statusDiv.innerHTML="Connected to " + macAddress + ".";        
     }, 
     onDisconnect: function() {
-        alert("Disconnected");
-        statusDiv.innerHTML="Disconnected.";
+        alert("Bluetooth Desconectado");
+        // statusDiv.innerHTML="Disconnected.";
     },
     onMessage: function(data) {
-        counter.innerHTML = data;        
+        // counter.innerHTML = data;        
     },
     subscribeFailed: function() {
-        alert("subscribe failed");
-    }
+        alert("Fallo la conexión al Bluetooth Arduino");
+    }, 
+    // From here JAD FUNCTIONS SPECIFIC FOT THIS PROJECT
+    setListeners: function() {
+        let activarBtn = document.querySelector("#activarBtn");
+        let desactivarBtn = document.querySelector("#desactivarBtn");
+        let pass = document.getElementById("pass"); // password input
+        let statusLabel = document.getElementById("statusLabel");
+
+        activarBtn.addEventListener('click', () => {
+            checkSystem(pass.value);
+        } , false);
+    }, 
+ 
 }; // END of APP
 
 document.addEventListener('DOMContentLoaded', function (){
     FastClick.attach(document.body); // smoother clicks on mobile dev
     app.initialize();
 }, false);
+
+function checkSystem(password){
+    //checks the password and connection to BT
+    console.log(`The password you've entered is: ${password}`);
+    if (password.toLowerCase() === 'julio'){
+        console.log("password correct");
+        bluetoothSerial.connect(macAddress, app.onConnect, app.onDisconnect);
+    }
+}
+
+function activateAllSystems(params) {
+    setStatusLabel("Sistema Conectado", "red");
+}
+
+function setStatusLabel(msg, color){
+    statusLabel.innerText = msg;
+    statusLabel.style.color = color;
+}
