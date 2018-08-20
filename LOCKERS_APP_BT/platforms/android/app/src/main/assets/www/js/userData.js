1ffe4b34f3d5967cf7080091bb6d42e620f2c4d9
JHIMMY ASTORAQUE DURAN <jadsa.corp2@gmail.com>
2018-08-20 12:30:33 -0400

    Added fill user data and pictures and userData.js file

---
+++ LOCKERS_APP_BT/platforms/android/app/src/main/assets/www/js/userData.js
@@ -0,0 +1,30 @@
+let usuarios =[ 
+    {
+        id: 1,
+        nombre : "Julio Mario Grondona",
+        carrera : "Ingeniería Industrial",
+        registro: "212033409",
+        foto : "mario.jpg"
+    },
+    {
+        id: 2,
+        nombre : "Ali Lemke",
+        carrera : "Ingeniería Electrica",
+        registro: "212033409",
+        foto : "person.jpg"
+    },
+    {
+        id: 3,
+        nombre : "Joanne Gerlach",
+        carrera : "Ingeniería Comercial",
+        registro: "212033409",
+        foto : "rolo.jpg"
+    },
+    {
+        id: 4,
+        nombre : "Frances Deckow",
+        carrera : "Ingeniería ",
+        registro: "212033409",
+        foto : "mario.jpg"
+    }    
+];
